11f1aaa968f6c12143ca8fc794fb79bfada24916
FredProbst <github@fsd.probst.org>
2023-02-23 18:32:24 -0500

    adapts Vector class to the 3D coordinate plane
    This commit adapts the vector class to allow for 3D vectors
    and 3D vector math. This replaces the Point3D class.

--- src/main/java/frc/robot/helpers/ArmSegmentHelper.java
+++ src/main/java/frc/robot/helpers/ArmSegmentHelper.java
@@ -1,6 +1,6 @@
 package frc.robot.helpers;
 import java.awt.geom.Point2D;
-import frc.robot.helpers.Point3D;
+import frc.robot.Vector;
 import frc.robot.subsystems.Arm;
 
 
@@ -18,7 +18,7 @@ public class ArmSegmentHelper {
     // TODO: replace with accurate values
     public static final double SHOULDER_LENGTH = 2; // in feet
     public static final double FOREARM_LENGTH = 1.4; // in feet
-    public static final Point3D SHOULDER_JOINT_COOR = new Point3D(0,0,1);
+    public static final Vector SHOULDER_JOINT_COOR = new Vector(0,0,1);
     // Used for the overextention prevention
     // TODO: replace with accurate values
     public static final double ROBOT_WIDTH = 2;
@@ -38,7 +38,7 @@ public class ArmSegmentHelper {
     public static final double LOOK_AHEAD_TIME = .25; // seconds
 
     
-    private Point3D m_handCoor;
+    private Vector m_handCoor;
 
     Arm m_arm;
 
@@ -56,38 +56,38 @@ public class ArmSegmentHelper {
         m_arm = Arm.getInstance();
         // TODO: Initialize with a more accurate coordinate.
         // Initially has m_handCoor set as if the arm is straight up with the forearm folded down, may want to i
-        m_handCoor = new Point3D(0,0, SHOULDER_LENGTH - FOREARM_LENGTH + SHOULDER_JOINT_COOR.getZ());
+        m_handCoor = new Vector(0,0, SHOULDER_LENGTH - FOREARM_LENGTH + SHOULDER_JOINT_COOR.z);
     }
 
     /**
      * This method calculates the 3D coordinate of the hand based on the current joint angles.
-     * @return A Point3D object representing the 3D cooridnate of the hand
+     * @return A Vector object representing the 3D cooridnate of the hand
      */
-    public Point3D calculateHandCoor() {
+    public Vector calculateHandCoor() {
         // TODO: Check origin of angle measurment and adjust to match with coordinate system
-        double x = SHOULDER_JOINT_COOR.getX() + Math.cos(Math.toRadians(m_arm.getTurretAngle())) *
+        double x = SHOULDER_JOINT_COOR.x + Math.cos(Math.toRadians(m_arm.getTurretAngle())) *
             (SHOULDER_LENGTH * Math.cos(Math.toRadians(m_arm.getShoulderAngle())) 
             + FOREARM_LENGTH * Math.cos(Math.toRadians(m_arm.getShoulderAngle() + m_arm.getElbowAngle() + 180)));
             
 
-        double y = SHOULDER_JOINT_COOR.getY() + Math.sin(Math.toRadians(m_arm.getTurretAngle())) *
+        double y = SHOULDER_JOINT_COOR.y + Math.sin(Math.toRadians(m_arm.getTurretAngle())) *
             (SHOULDER_LENGTH * Math.cos(Math.toRadians(m_arm.getShoulderAngle())) 
             + FOREARM_LENGTH * Math.cos(Math.toRadians(m_arm.getShoulderAngle() + m_arm.getElbowAngle() + 180)));
 
 
-        double z = SHOULDER_JOINT_COOR.getZ() 
+        double z = SHOULDER_JOINT_COOR.z 
             + SHOULDER_LENGTH * Math.sin(Math.toRadians(m_arm.getShoulderAngle())) 
             + FOREARM_LENGTH * Math.sin(Math.toRadians(m_arm.getShoulderAngle() + m_arm.getElbowAngle() + 180));
 
-        m_handCoor = new Point3D(x,y,z);
+        m_handCoor = new Vector(x,y,z);
         return m_handCoor;
     }
 
     /**
      * This method calculates the velocity of the hand based on the current angles and velocities of the joints
-     * @return A Point3D object representing the 3D velocity vector of the hand.
+     * @return A Vector object representing the 3D velocity vector of the hand.
      */
-    public Point3D calculateHandVelocity() { // returns measurement in feet per second
+    public Vector calculateHandVelocity() { // returns measurement in feet per second
         // TODO: Test if hand velocity calculations are accurate. May want to take derivative of the equation that calculates the coordinates.
         // TODO: Check origin of angle measurment and adjust to match with coordinate system
         double XYDistanceFromJointToHand = (SHOULDER_LENGTH * Math.cos(Math.toRadians(m_arm.getShoulderAngle())) 
@@ -107,45 +107,45 @@ public class ArmSegmentHelper {
         // Adds velocity contributed by both arm segments
         double handVelZ = elbowVelZ + (m_arm.getElbowVelocity() * RPM_TO_RAD * FOREARM_LENGTH * Math.cos(Math.toRadians(m_arm.getElbowAngle())));
         
-        return new Point3D(handVelX,handVelY,handVelZ);
+        return new Vector(handVelX,handVelY,handVelZ);
     }
 
     /**
      * This method takes an input coordinate and tests whether it has is outside the boundries of the arm movement
-     * @param testCoor A Point3D object representing the coordinate to be tested
+     * @param testCoor A Vector object representing the coordinate to be tested
      * @return         An enum indicating the side of the robot the robot has overextended
      */
-    public extentionLimitFaces checkForOverextention(Point3D testCoor) {
+    public extentionLimitFaces checkForOverextention(Vector testCoor) {
         // TODO: If overextended, return Minimum Translation Vector (the smallest vector required to translate the hand back into legal space)
-        if (testCoor.getX() >= FRONT_EXTENTION_LIMIT) {
+        if (testCoor.x >= FRONT_EXTENTION_LIMIT) {
             return extentionLimitFaces.FRONT;
-        } else if (testCoor.getX() <= BACK_EXTENTION_LIMIT) {
+        } else if (testCoor.x <= BACK_EXTENTION_LIMIT) {
             return extentionLimitFaces.BACK;
         }
 
-        if (testCoor.getY() >= RIGHT_EXTENTION_LIMIT) {
+        if (testCoor.y >= RIGHT_EXTENTION_LIMIT) {
             return extentionLimitFaces.RIGHT;
-        } else if (testCoor.getY() <= LEFT_EXTENTION_LIMIT) {
+        } else if (testCoor.y <= LEFT_EXTENTION_LIMIT) {
             return extentionLimitFaces.LEFT;
         }
 
-        if (testCoor.getZ() >= UPPER_EXTENTION_LIMIT) {
+        if (testCoor.z >= UPPER_EXTENTION_LIMIT) {
             return extentionLimitFaces.TOP;
-        } else if (testCoor.getZ() <= LOWER_EXTENTION_LIMIT) {
+        } else if (testCoor.z <= LOWER_EXTENTION_LIMIT) {
             return extentionLimitFaces.BOTTOM;
         }
         return extentionLimitFaces.NONE;
     }
 
-    public Point3D calculateFutureCoor(double lookAheadTime) {
+    public Vector calculateFutureCoor(double lookAheadTime) {
         // TODO: Track the elbow joint coordinate
         // Currently only looks at the coordinate and velocity of the hand.
         calculateHandCoor();
-        Point3D m_currentVelocity = calculateHandVelocity();
-        return new Point3D(
-            m_handCoor.getX() + m_currentVelocity.getX() * lookAheadTime, 
-            m_handCoor.getY() + m_currentVelocity.getY() * lookAheadTime, 
-            m_handCoor.getZ() + m_currentVelocity.getZ() * lookAheadTime);
+        Vector m_currentVelocity = calculateHandVelocity();
+        return new Vector(
+            m_handCoor.x + m_currentVelocity.x * lookAheadTime, 
+            m_handCoor.y + m_currentVelocity.y * lookAheadTime, 
+            m_handCoor.z + m_currentVelocity.z * lookAheadTime);
     }
 
     /**
